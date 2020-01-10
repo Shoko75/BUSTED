a155cb0e1808d471697f909c4349c6e497520869
kosho75 <kosho75@gmail.com>
2020-01-09 22:11:42 -0800

    [ FriendsList ] added the logic for showing the userImage

--- Cops and Robbers/Controller/Components.swift
+++ Cops and Robbers/Controller/Components.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+// MARK: UIViewController extension
 extension UIViewController {
     
     func showAlert(title: String, message: String){
@@ -24,5 +25,40 @@ extension UIViewController {
         }
         
         present(alert, animated: true, completion: nil)
+    }
+}
+
+// MARK: UIImageView extension
+let imageCache = NSCache<AnyObject, AnyObject>()
+extension UIImageView {
+    
+    func loadImageUsingCacheWithUrlString(urlString: String) {
+        
+        self.image = nil
+        
+        // check cache wether image is aleady exsist
+        if let cachedImage = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+        
+        // otherwise fire off a new download
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            DispatchQueue.global(qos: .background).async {
+                DispatchQueue.main.async {
+                    
+                    if let downloadedImage = UIImage(data: data!) {
+                        imageCache.setObject(downloadedImage, forKey: urlString as AnyObject)
+                        self.image = downloadedImage
+                    }
+                }
+            }
+        }).resume()
     }
 }
