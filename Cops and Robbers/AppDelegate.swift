a21efe08f61043110ebe9f9f57b3ee16108e3f4c
kosho75 <kosho75@gmail.com>
2020-01-22 16:48:51 -0800

    test

--- Cops and Robbers/AppDelegate.swift
+++ Cops and Robbers/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var locationManager: CLLocationManager?
     var notificationCenter: UNUserNotificationCenter?
     let gcmMessageIDKey = "gcm.message_id"
+    let aa = ""
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        
