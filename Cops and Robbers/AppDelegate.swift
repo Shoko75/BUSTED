dd64cc98efe2b88e0a4dc7fdc5539bd14627408b
kosho75 <kosho75@gmail.com>
2020-01-22 19:13:55 -0800

    fix conflict

--- Cops and Robbers/AppDelegate.swift
+++ Cops and Robbers/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var locationManager: CLLocationManager?
     var notificationCenter: UNUserNotificationCenter?
     let gcmMessageIDKey = "gcm.message_id"
-    let aa = ""
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        
