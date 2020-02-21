921f6fa891b04f28ba7bfab6aaee028d5251fe2a
kosho75 <kosho75@gmail.com>
2020-02-21 15:29:36 -0800

    [ EndGame ] Added EndGame page

---
+++ Cops and Robbers/Controller/EndGameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EndGameViewController.swift
+//  Cops and Robbers
+//
+//  Created by Shoko Hashimoto on 2020-02-21.
+//  Copyright © 2020 Shoko Hashimoto. All rights reserved.
+//
+
+import UIKit
+
+class EndGameViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var menuButton: UIButton!
+    
+    var winCopsFlg: Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func pressedMenu(_ sender: Any) {
+    }
+    
+}
