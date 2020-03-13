677fa14c3f371ed18dcfc23a8ce8d00dcf63a569
kosho75 <kosho75@gmail.com>
2020-03-13 00:59:13 -0700

    Changed menu and how to play design

--- Cops and Robbers/Model/HowToPlayData.swift
+++ Cops and Robbers/Model/HowToPlayData.swift
@@ -8,13 +8,13 @@
 
 struct HowToPlayData {
 
-    let data = [["ImageName": "Fortunewheel",
+    let data = [["ImageName": "how_to_play_fortunewheel",
                  "text": "Players are randomly selected to be in a team of  Cops or Robbers"],
 
-                ["ImageName": "map",
+                ["ImageName": "how_to_play_map",
                 "text": "Once your team is selected, robbers have the opportunity to run or hide for 30 seconds before the game starts."],
 
-                ["ImageName": "police chase",
+                ["ImageName": "cop_chasing_robber",
                 "text": "If you’re a cop, your objective is to catch a robber by keeping a close distance of 1 meter."],
     
                 ["ImageName": "robber",
@@ -23,7 +23,7 @@ struct HowToPlayData {
                 ["ImageName": "phone_alert",
                 "text": "If a cop or a robber is nearby,  you will both be alerted. Try your best to chase or run away :)"],
     
-                ["ImageName": "trespass",
+                ["ImageName": "robber_trespassing",
                 "text": "Do not trespass or enter dangerous areas while playing Busted."],
                 
                 ["ImageName": "throw",
